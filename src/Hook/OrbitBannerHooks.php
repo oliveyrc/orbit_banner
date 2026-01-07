bd1d18ce0ec5b4809bb86ffc7aa31540921d5c9d
Richard Olivey <richard@olivey.net>
2026-01-07 21:57:15 +0000

    More help text

--- src/Hook/OrbitBannerHooks.php
+++ src/Hook/OrbitBannerHooks.php
@@ -4,14 +4,17 @@ declare(strict_types=1);
 
 namespace Drupal\orbit_banner\Hook;
 
-use Drupal\Core\Routing\RouteMatchInterface;
 use Drupal\Core\Hook\Attribute\Hook;
+use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
 
 /**
  * Hook implementations for the Orbit Banner module.
  */
 class OrbitBannerHooks
 {
+  use StringTranslationTrait;
+
   /**
    * Implements hook_help().
    */
@@ -21,8 +24,8 @@ class OrbitBannerHooks
     switch ($route_name) {
       case 'help.page.orbit_banner':
         $output = '';
-        $output .= '<h3>' . t('About') . '</h3>';
-        $output .= '<p>' . t('This module provides banner functionality, in the form of a block and creates the required fields on a content type.') . '</p>';
+        $output .= '<h3>' . $this->t('About') . '</h3>';
+        $output .= '<p>' . $this->t('This module provides banner functionality, in the form of a block and creates the required fields on a content type.') . '</p>';
 
         return $output;
     }
